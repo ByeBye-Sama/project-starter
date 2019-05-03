0d5d7febd730fa6e11099eeab5f7478ed344c284
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-05-03 18:15:42 -0500

    Start react base template

---
+++ React/base/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './assets/styles/main.scss';
+
+export function App() {
+  return (
+    <div className="Wrapper">
+      React is now working!!
+    </div>
+  );
+}
---
+++ React/base/webpack.base.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: ["babel-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'React',
+    template: path.resolve(__dirname, 'index.html')
+  })]
+};
